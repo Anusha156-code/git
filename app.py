f25effb4ec381b9907cac17f9f9527322eec0cb6
Anusha <anusha4615@gmail.com>
2026-05-08 08:09:26 +0530

    4 committteetyuio

--- app.py
+++ app.py
@@ -2,8 +2,5 @@
 
 print("hello world, and so many programs learnt in clg,and devops and cloud")
 
-print("hello world, and so many programs learnt in clg,and devops and cloud ")
 
-
-print("hello world, and so many programs learnt in clg,and devops and cloud ")
 
