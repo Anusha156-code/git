074232dc0b9fa6a07a0199fc365ffd5e4dd53093
Anusha <anusha4615@gmail.com>
2026-05-08 08:40:10 +0530

    committee

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 
-print("hello world, and so many programs learnt in clg,and devops and cloud")
+print("hello world, and so many programs learnt in clg,and devops and cloud and it will be fun  ")
 
 
 
