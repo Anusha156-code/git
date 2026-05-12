6e26cfdb0c88bde2caa2ca42e79c5f2066070d10
Anusha <anusha4615@gmail.com>
2026-05-12 07:39:04 +0530

    commit after merge conflict

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 
 
 
-print("hello world, and so many programs learnt in clg,and devops and cloud and working on something new hello world, and so many programs learnt in clg,and devops and cloud using github ")
+
+print("hello world, and so many programs learnt in clg,and devops and cloud and working on something new hello world, and so many programs learnt in clg,and devops and cloud using github git hello world, and so many programs learnt in clg,and devops and cloud and it will be fun and more productive and ...so on..anddd ...........and........... ")
 
 
 
